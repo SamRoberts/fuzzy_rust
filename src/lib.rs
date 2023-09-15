1597cd6d1b406661504bdfb27fb8c0a55eaa34e1
Sam Roberts <sam.roberts.1983@gmail.com>
2023-09-15 12:11:00 +1000

    First algorithm that appears to be successful

--- src/lib.rs
+++ src/lib.rs
@@ -8,54 +8,6 @@ use std::collections::hash_map::HashMap;
 // And the parser will ensure that all groups are balanced
 // So our algorithm does not have to worry about having more "starts" than "ends"
 
-fn score(problem: &Problem) -> State {
-    let mut state = State::new();
-    let _ = score_impl(problem, &mut state, problem.start_ix());
-    state
-}
-
-fn score_impl(problem: &Problem, state: &mut State, ix: Ix) -> Edge {
-    match state.nodes.get(&ix) {
-        Some(Node::Working) =>
-            // We will enter an infinite loop if we go here.
-            // However, this path may still be valid in the end: we
-            // just don't know what it costs yet. So return an eddge that
-            // depends on this node.
-            Edge::Depends { cost: 0, node: ix },
-        Some(Node::Done { score, next }) =>
-            // We've already done this ix: stop here.
-            Edge::Score { score: *score, next: ix },
-        None => {
-            state.nodes.insert(ix, Node::Working);
-            let steps = problem.succ(ix);
-
-            let best_edge = steps.iter()
-                .map(|step| score_impl(problem, state, step.ix()).add_cost(step.cost))
-                .reduce(Edge::combine)
-                .and_then(|combined| combined.remove_loop(ix));
-
-            match best_edge {
-                None => {
-                    assert!(ix == problem.end_ix(), "No legal moves at {:?}", ix);
-                    state.nodes.insert(ix, Node::Done { score: 0, next: ix });
-                    Edge::Score { score: 0, next: ix }
-                },
-                Some(Edge::Score { score , next }) => {
-                    state.nodes.insert(ix, Node::Done { score, next });
-                    Edge::Score { score , next: ix }
-                },
-                Some(Edge::Depends { cost, node }) => {
-                    state.nodes.remove(&ix);
-                    Edge::Depends { cost, node }
-                },
-                Some(Edge::DependsOrScore { cost, node, alt_score, alt_next }) => {
-                    state.nodes.remove(&ix);
-                    Edge::DependsOrScore { cost, node, alt_score, alt_next: ix }
-                },
-            }
-        }
-    }
-}
 
 #[derive(Eq, PartialEq, Copy, Clone, Debug)]
 enum Patt {
@@ -74,103 +26,126 @@ enum Text {
     End
 }
 
-#[derive(Eq, PartialEq, Copy, Clone, Hash, Debug)]
+#[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash, Debug)]
 struct Ix {
+    // There is a separate score associated with each combination of:
+    //   1. the place we are up to in the pattern
+    //   2. the place we are up to in the text
+    //   3. how many kleene patterns we have passed into since we last made
+    //      progress in the text. It is never beneficial to backtrack
+    //      to the start of a kleene group if we haven't progressed through
+    //      the text since starting that kleene.
+
+    // TODO let's change these ix names later ...
     pix: usize,
     tix: usize,
+    kix: usize,
 }
 
 #[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash, Debug)]
 struct Step {
     cost: usize,
-    pix: usize,
-    tix: usize,
+    next: Ix,
 }
 
-impl Step {
-    fn forward(cost: usize, ix: Ix, poff: usize, toff: usize) -> Step {
-        Step { cost: cost, pix: ix.pix + poff, tix: ix.tix + toff }
-    }
-
-    fn back(cost: usize, ix: Ix, poff: usize, toff: usize) -> Step {
-        Step { cost: cost, pix: ix.pix - poff, tix: ix.tix - toff }
-    }
-
-    fn ix(&self) -> Ix {
-        Ix { pix: self.pix, tix: self.tix }
-    }
+#[derive(Eq, PartialEq, Ord, PartialOrd, Copy, Clone, Hash, Debug)]
+struct Score {
+    score: usize,
+    next: Ix,
 }
 
 #[derive(Eq, PartialEq, Copy, Clone, Debug)]
 enum Node {
     Working,
-    Done { score: usize, next: Ix },
-}
-
-#[derive(Copy, Clone, Debug)]
-enum Edge {
-    Score { score: usize , next: Ix },
-    Depends { cost: usize, node: Ix },
-    // Either depends path or path with known score might be viable
-    // alt_score should be > cost
-    DependsOrScore { cost: usize, node: Ix, alt_score: usize, alt_next: Ix },
-}
-
-impl Edge {
-    fn combine(left: Self, right: Self) -> Self {
-        match (left, right) {
-            (Edge::Score { score: score_l , .. }, Edge::Score { score: score_r, .. }) if score_l <= score_r =>
-                left,
-            (Edge::Score { score: score_l , .. }, Edge::Score { score: score_r, .. }) if score_l > score_r =>
-                right,
-            (Edge::Score { score, .. }, Edge::Depends { cost, .. }) if score <= cost =>
-                left,
-            (Edge::Score { score, next }, Edge::Depends { cost, node }) if score > cost =>
-                Edge::DependsOrScore { cost: cost, node: node, alt_score: score, alt_next: next },
-            (Edge::Score { score, ..}, Edge::DependsOrScore { cost, .. }) if score <= cost =>
-                left,
-            (Edge::Score { score, .. }, Edge::DependsOrScore { alt_score, .. }) if score > alt_score =>
-                right,
-            (Edge::Score { score, next }, Edge::DependsOrScore { cost, node, .. }) => 
-                Edge::DependsOrScore { cost: cost, node: node, alt_score: score, alt_next: next },
-            (Edge::Depends { .. }, Edge::Score { .. }) =>
-                Self::combine(right, left),
-            (Edge::DependsOrScore { .. }, Edge::Score { .. }) =>
-                Self::combine(right, left),
-            _ =>
-                // Ok, so we do hit this condition with nested kleenes:
-                // need to understand how, as I thought this would only happen if kleene groups were interleaved
-                panic!("Cannot handle multiple depends edges {:?} and {:?}", left, right),
-        }
-    }
-
-    fn remove_loop(&self, current: Ix) -> Option<Edge> {
-        match self {
-            Edge::Depends { node, .. } if *node == current =>
-                None,
-            Edge::DependsOrScore { node, alt_score, alt_next, .. } if *node == current =>
-                Some(Edge::Score { score: *alt_score, next: *alt_next }),
-            _ =>
-                Some(*self)
-        }
-    }
-
-    fn add_cost(&self, extra: usize) -> Edge {
-        match self {
-            Edge::Score { score, next } => Edge::Score { score: *score + extra, next: *next },
-            Edge::Depends { cost, node } => Edge::Depends { cost: *cost + extra, node: *node },
-            Edge::DependsOrScore { cost, node, alt_score, alt_next } => Edge::DependsOrScore {
-                cost: *cost + extra,
-                node: *node,
-                alt_score: *alt_score + extra,
-                alt_next: *alt_next
-            }
-        }
-    }
+    Done(Score),
 }
 
 struct State {
   nodes: HashMap<Ix, Node>,
+}
+
+struct Problem {
+  pattern: Vec<Patt>,
+  text: Vec<Text>,
+}
+
+fn score(problem: &Problem) -> State {
+    let mut state = State::new();
+    let _ = score_impl(problem, &mut state, problem.start_ix());
+    state
+}
+
+fn score_impl(problem: &Problem, state: &mut State, ix: Ix) -> Score {
+    match state.nodes.get(&ix) {
+        Some(Node::Working) =>
+            panic!("About to enter an infinite loop at {:?}", ix),
+        Some(Node::Done(score)) =>
+            Score { score: score.score, next: ix },
+        None => {
+            state.nodes.insert(ix, Node::Working);
+            let steps = problem.succ(ix);
+
+            let maybe_score = steps.iter()
+                .map(|step| score_impl(problem, state, step.next).add_cost(step.cost))
+                .reduce(Score::combine);
+
+            let score = maybe_score.unwrap_or_else(|| {
+                assert!(ix == problem.end_ix(), "No legal moves at {:?}", ix);
+                Score { score: 0, next: problem.end_ix() }
+            });
+
+            state.nodes.insert(ix, Node::Done(score));
+            Score { score: score.score, next: ix }
+        }
+    }
+}
+
+impl Step {
+    fn new(cost: usize, pix: usize, tix: usize, kix: usize) -> Self {
+        Step { cost, next: Ix { pix, tix, kix } }
+    }
+
+    fn skip_text(ix: Ix) -> Self {
+        Step { cost: 1, next: Ix { tix: ix.tix + 1, kix: 0, ..ix } }
+    }
+
+    fn skip_patt(ix: Ix) -> Self {
+        Step { cost: 1, next: Ix { pix: ix.pix + 1, ..ix } }
+    }
+
+    fn hit(ix: Ix) -> Self {
+        Step { cost: 0, next: Ix { pix: ix.pix + 1, tix: ix.tix + 1, kix: 0 } }
+    }
+
+    fn pass_group(ix: Ix) -> Self {
+        Step { cost: 0, next: Ix { pix: ix.pix + 1, ..ix } }
+    }
+
+    fn start_kleene(ix: Ix) -> Self {
+        Step { cost: 0, next: Ix { pix: ix.pix + 1, kix: ix.kix + 1, ..ix } }
+    }
+
+    fn end_kleene(ix: Ix) -> Self {
+        Step { cost: 0, next: Ix { pix: ix.pix + 1, kix: ix.kix - 1, ..ix } }
+    }
+
+    fn pass_kleene(ix: Ix, off: usize) -> Self {
+        Step { cost: 0, next: Ix { pix: ix.pix + 1 + off, ..ix } }
+    }
+
+    fn restart_kleene(ix: Ix, off: usize) -> Self {
+        Step { cost: 0, next: Ix { pix: ix.pix - off, ..ix } }
+    }
+}
+
+impl Score {
+    fn add_cost(&self, cost: usize) -> Score {
+        Score { score: self.score + cost, ..*self }
+    }
+
+    fn combine(left: Self, right: Self) -> Self {
+        if left.score <= right.score { left } else { right }
+    }
 }
 
 impl State {
@@ -182,7 +157,7 @@ impl State {
 
     fn score(&self, problem: &Problem) -> Option<usize> {
         match self.nodes.get(&problem.start_ix()) {
-            Some(Node::Done { score, .. }) => Some(*score),
+            Some(Node::Done(Score { score, .. })) => Some(*score),
             _ => None,
         }
     }
@@ -190,7 +165,7 @@ impl State {
     fn trace(&self, problem: &Problem) -> Option<Vec<Ix>> {
         let mut optimal = vec![];
         let mut ix = problem.start_ix();
-        while let Some(Node::Done { score, next }) = self.nodes.get(&ix) {
+        while let Some(Node::Done(Score { next, .. })) = self.nodes.get(&ix) {
             if ix == problem.end_ix() {
                 return Some(optimal);
             }
@@ -201,60 +176,55 @@ impl State {
     }
 }
 
-struct Problem {
-  pattern: Vec<Patt>,
-  text: Vec<Text>,
-}
-
 impl Problem {
     fn start_ix(&self) -> Ix {
-        Ix { pix: 0, tix: 0}
+        Ix { pix: 0, tix: 0, kix: 0 }
     }
 
     fn end_ix(&self) -> Ix {
-        Ix { pix: self.pattern.len() - 1, tix: self.text.len() - 1 }
+        Ix { pix: self.pattern.len() - 1, tix: self.text.len() - 1, kix: 0 }
     }
 
     fn succ(&self, ix: Ix) -> Vec<Step> {
         let patt = self.pattern[ix.pix];
         let text = self.text[ix.tix];
 
-        let mut scores = vec![];
+        let mut steps = vec![];
 
         match (patt, text) {
             (Patt::Any, Text::Lit(_)) =>
-                scores.push(Step::forward(0, ix, 1, 1 )),
+                steps.push(Step::hit(ix)),
             (Patt::Lit(a), Text::Lit(b)) if a == b =>
-                scores.push(Step::forward(0, ix, 1, 1 )),
+                steps.push(Step::hit(ix)),
             _ =>
                 (),
         }
 
         match text {
             Text::Lit(_) =>
-                scores.push(Step::forward(1, ix, 0, 1)),
+                steps.push(Step::skip_text(ix)),
             Text::End =>
                 (),
         }
 
         match patt {
             Patt::Lit(_) | Patt::Any =>
-                scores.push(Step::forward(1, ix, 1, 0)),
+                steps.push(Step::skip_patt(ix)),
             Patt::GroupStart | Patt::GroupEnd =>
-                scores.push(Step::forward(0, ix, 1, 0)),
-            Patt::KleeneEnd(off) => {
-                scores.push(Step::forward(0, ix, 1, 0));
-                scores.push(Step::back(0, ix, off, 0));
-            }
+                steps.push(Step::pass_group(ix)),
+            Patt::KleeneEnd(_) if ix.kix > 0 =>
+                steps.push(Step::end_kleene(ix)),
+            Patt::KleeneEnd(off) => // ix.kix == 0
+                steps.push(Step::restart_kleene(ix, off)),
             Patt::KleeneStart(off) => {
-                scores.push(Step::forward(0, ix, 1, 0));
-                scores.push(Step::forward(0, ix, off + 1, 0));
+                steps.push(Step::start_kleene(ix));
+                steps.push(Step::pass_kleene(ix, off));
             }
             Patt::End =>
                 (),
         }
 
-        scores
+        steps
     }
 }
 
@@ -350,7 +320,18 @@ mod tests {
     }
 
     fn p_all() -> Vec<Problem> {
-        vec![p_match_empty(), p_match_lit_1(), p_match_kleene_1()]
+        vec![
+            p_match_empty(),
+            p_match_lit_1(),
+            p_match_lit_2(),
+            p_match_kleene_1(),
+            p_match_kleene_2(),
+            p_fail_empty_1(),
+            p_fail_empty_2(),
+            p_fail_lit_1(),
+            p_fail_lit_2(),
+            p_fail_kleene_1(),
+        ]
     }
 
     #[test]
@@ -367,7 +348,7 @@ mod tests {
         let p = p_match_lit_1();
         let state = score(&p);
         let expected = Some(vec![
-            Ix { pix: 1, tix: 1 },
+            Ix { pix: 1, tix: 1, kix: 0 },
         ]);
         let actual = state.trace(&p);
         assert_eq!(expected, actual);
@@ -378,8 +359,8 @@ mod tests {
         let p = p_match_lit_2();
         let state = score(&p);
         let expected = Some(vec![
-            Ix { pix: 1, tix: 1 },
-            Ix { pix: 2, tix: 2 },
+            Ix { pix: 1, tix: 1, kix: 0 },
+            Ix { pix: 2, tix: 2, kix: 0 },
         ]);
         let actual = state.trace(&p);
         assert_eq!(expected, actual);
@@ -390,12 +371,13 @@ mod tests {
         let p = p_match_kleene_1();
         let state = score(&p);
         let expected = Some(vec![
-            Ix { pix: 1, tix: 0 },
-            Ix { pix: 2, tix: 1 },
-            Ix { pix: 0, tix: 1 },
-            Ix { pix: 1, tix: 1 },
-            Ix { pix: 2, tix: 2 },
-            Ix { pix: 3, tix: 2 },
+            Ix { pix: 1, tix: 0, kix: 1 },
+            Ix { pix: 2, tix: 1, kix: 0 },
+            Ix { pix: 0, tix: 1, kix: 0 },
+            Ix { pix: 1, tix: 1, kix: 1 },
+            Ix { pix: 2, tix: 2, kix: 0 },
+            Ix { pix: 0, tix: 2, kix: 0 },
+            Ix { pix: 3, tix: 2, kix: 0 },
         ]);
         let actual = state.trace(&p);
         assert_eq!(expected, actual);
@@ -406,12 +388,28 @@ mod tests {
         let p = p_match_kleene_2();
         let state = score(&p);
         let expected = Some(vec![
-            Ix { pix: 1, tix: 0 },
-            Ix { pix: 2, tix: 1 },
-            Ix { pix: 0, tix: 1 },
-            Ix { pix: 1, tix: 1 },
-            Ix { pix: 2, tix: 2 },
-            Ix { pix: 3, tix: 2 },
+            Ix { pix: 1, tix: 0, kix: 1 },
+            Ix { pix: 2, tix: 1, kix: 0 },
+            Ix { pix: 5, tix: 1, kix: 0 },
+            Ix { pix: 0, tix: 1, kix: 0 },
+            Ix { pix: 1, tix: 1, kix: 1 },
+            Ix { pix: 2, tix: 2, kix: 0 },
+            Ix { pix: 3, tix: 2, kix: 1 },
+            Ix { pix: 4, tix: 3, kix: 0 },
+            Ix { pix: 2, tix: 3, kix: 0 },
+            Ix { pix: 5, tix: 3, kix: 0 },
+            Ix { pix: 0, tix: 3, kix: 0 },
+            Ix { pix: 1, tix: 3, kix: 1 },
+            Ix { pix: 2, tix: 4, kix: 0 },
+            Ix { pix: 3, tix: 4, kix: 1 },
+            Ix { pix: 4, tix: 5, kix: 0 },
+            Ix { pix: 2, tix: 5, kix: 0 },
+            Ix { pix: 3, tix: 5, kix: 1 },
+            Ix { pix: 4, tix: 6, kix: 0 },
+            Ix { pix: 2, tix: 6, kix: 0 },
+            Ix { pix: 5, tix: 6, kix: 0 },
+            Ix { pix: 0, tix: 6, kix: 0 },
+            Ix { pix: 6, tix: 6, kix: 0 },
         ]);
         let actual = state.trace(&p);
         assert_eq!(expected, actual);
@@ -422,7 +420,7 @@ mod tests {
         let p = p_fail_empty_1();
         let state = score(&p);
         let expected = Some(vec![
-            Ix { pix: 0, tix: 1 },
+            Ix { pix: 0, tix: 1, kix: 0 },
         ]);
         let actual = state.trace(&p);
         assert_eq!(expected, actual);
@@ -433,7 +431,7 @@ mod tests {
         let p = p_fail_empty_2();
         let state = score(&p);
         let expected = Some(vec![
-            Ix { pix: 1, tix: 0 },
+            Ix { pix: 1, tix: 0, kix: 0 },
         ]);
         let actual = state.trace(&p);
         assert_eq!(expected, actual);
@@ -444,8 +442,8 @@ mod tests {
         let p = p_fail_lit_1();
         let state = score(&p);
         let expected = Some(vec![
-            Ix { pix: 1, tix: 1 },
-            Ix { pix: 1, tix: 2 },
+            Ix { pix: 1, tix: 1, kix: 0 },
+            Ix { pix: 1, tix: 2, kix: 0 },
         ]);
         let actual = state.trace(&p);
         assert_eq!(expected, actual);
@@ -456,9 +454,9 @@ mod tests {
         let p = p_fail_lit_2();
         let state = score(&p);
         let expected = Some(vec![
-            Ix { pix: 1, tix: 1 },
-            Ix { pix: 2, tix: 1 },
-            Ix { pix: 3, tix: 2 },
+            Ix { pix: 1, tix: 1, kix: 0 },
+            Ix { pix: 2, tix: 1, kix: 0 },
+            Ix { pix: 3, tix: 2, kix: 0 },
         ]);
         let actual = state.trace(&p);
         assert_eq!(expected, actual);
@@ -476,15 +474,15 @@ mod tests {
     #[test]
     fn problem_start_ix() {
         for p in p_all() {
-            assert_eq!(p.start_ix(), Ix { pix: 0, tix: 0 });
+            assert_eq!(p.start_ix(), Ix { pix: 0, tix: 0, kix: 0 });
         }
     }
 
     #[test]
     fn problem_end_ix() {
-        assert_eq!(p_match_empty().end_ix(), Ix { pix: 0, tix: 0 });
-        assert_eq!(p_match_lit_1().end_ix(), Ix { pix: 1, tix: 1 });
-        assert_eq!(p_match_kleene_1().end_ix(), Ix { pix: 3, tix: 2 });
+        assert_eq!(p_match_empty().end_ix(), Ix { pix: 0, tix: 0, kix: 0 });
+        assert_eq!(p_match_lit_1().end_ix(), Ix { pix: 1, tix: 1, kix: 0 });
+        assert_eq!(p_match_kleene_1().end_ix(), Ix { pix: 3, tix: 2, kix: 0 });
     }
 
     #[test]
@@ -498,9 +496,9 @@ mod tests {
     fn problem_succ_lit_match() {
         let p = p_match_lit_1();
         let expected = HashSet::from([
-            Step { cost: 0, pix: 1, tix: 1 },
-            Step { cost: 1, pix: 1, tix: 0 },
-            Step { cost: 1, pix: 0, tix: 1 },
+            Step::new(0, 1, 1, 0),
+            Step::new(1, 1, 0, 0),
+            Step::new(1, 0, 1, 0),
         ]);
         let actual = HashSet::from_iter(p.succ(p.start_ix()));
 
@@ -511,9 +509,9 @@ mod tests {
     fn problem_succ_kleene_start() {
         let p = p_match_kleene_1();
         let expected = HashSet::from([
-            Step { cost: 1, pix: 0, tix: 1 },
-            Step { cost: 0, pix: 1, tix: 0 },
-            Step { cost: 0, pix: 3, tix: 0 },
+            Step::new(1, 0, 1, 0),
+            Step::new(0, 1, 0, 1),
+            Step::new(0, 3, 0, 0),
         ]);
         let actual = HashSet::from_iter(p.succ(p.start_ix()));
 
@@ -524,11 +522,10 @@ mod tests {
     fn problem_succ_kleene_end() {
         let p = p_match_kleene_1();
         let expected = HashSet::from([
-            Step { cost: 1, pix: 2, tix: 1 },
-            Step { cost: 0, pix: 0, tix: 0 },
-            Step { cost: 0, pix: 3, tix: 0 },
+            Step::new(1, 2, 1, 0),
+            Step::new(0, 0, 0, 0),
         ]);
-        let actual = HashSet::from_iter(p.succ(Ix { pix: 2, tix: 0}));
+        let actual = HashSet::from_iter(p.succ(Ix { pix: 2, tix: 0, kix: 0}));
 
         assert_eq!(expected, actual);
     }
